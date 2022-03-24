d414ceb1bd4f4fdbba3c4c06b405ad9262257592
Daniel <danielnguyen10.uk@gmail.com>
2022-03-24 10:52:58 +0000

    changed base react app to something of my own

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">
+          You found a mokoko seed
+        </h1>
+        <ImageRenderer url="https://assets.maxroll.gg/wordpress/LA_Mokko_Seed.png" />
       </header>
     </div>
+    </>
   );
+}
+
+class ImageRenderer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        clicked : false
+      }
+    }
+
+    imageClick = () => {
+      let sound = new Audio("./MokokoSound.mp3")
+      this.setState({clicked : true});
+      sound.play()
+    }
+
+    render () {
+      const clicked = this.state.clicked;
+      if (clicked) {
+        return null;
+      }
+
+      return (
+        <img src={this.props.url} onClick={this.imageClick} alt="mokoko seed" className="App-displayImg"/>
+      );
+    };
+  
 }
 
 export default App;
