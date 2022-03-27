93359d96400a1bd2e7f5fc6e51a27dbcbe1aaa25
Daniel <danielnguyen10.uk@gmail.com>
2022-03-27 10:45:25 +0100

    Add basic table to MariShop

--- lost-ark-project/src/components/MariShop.js
+++ lost-ark-project/src/components/MariShop.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './MariShop.css'
 
-export const MariShop = () => {
-    return (
+export class MariShop extends Component {
+    render() {
+        return (
         <div>
-            <h1 className="App-header"> TODO </h1>
+            <ProductTable marketTable={marketTable}/>
         </div>
-    )
+        );
+    }
 }
+
+
+class ProductTable extends Component {
+    render() {
+        const rows = []
+        this.props.marketTable.map((item) => 
+            rows.push(<ProductRow item={item} />)
+        );
+        return (
+            <table className='table'>
+            <thead>
+            <tr>
+                <th>Price</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Verdict</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+        );
+    }
+}
+
+class ProductRow extends Component {
+    render() {
+        const item = this.props.item
+        return (
+            <tr key={item}>
+                <td>
+                    <form>
+                        <input type="text" placeholder='Enter price in market'/>
+                    </form>
+                </td>
+                <td className='td'>{item.name}</td>
+                <td className='td'>Bundle of {item.quantity}</td>
+                <td>
+                    <span style={{color: 'green'}}>
+                        Buy in mari
+                    </span>
+                </td>
+            </tr>
+        );
+    }
+}
+
+const marketTable = [
+    {
+        name: "Guardian Stone",
+        quantity: "10"
+    },
+    {
+        name: "Destruction Stone",
+        quantity: "10"
+    }
+];
